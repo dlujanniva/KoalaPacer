585973400d5ab1537a115cf4104f3fc3a1bda99a
Daniel Lujan <dlujan@niva.mx>
2015-05-28 17:34:37 -0500

    23,

--- app/src/main/java/dlv/niva/mx/koalapacer/FullscreenActivity.java
+++ app/src/main/java/dlv/niva/mx/koalapacer/FullscreenActivity.java
@@ -14,7 +14,7 @@ import android.view.View;
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
- *
+ *t
  * @see SystemUiHider
  */
 public class FullscreenActivity extends Activity {
